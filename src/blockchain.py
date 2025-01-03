d30cd3ba723aa6829ac475a0e8b21af0841a043a
i5irin <contact@i5ir.in>
2025-01-03 21:21:56 +0900

    feat: add a process to generate a block

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,11 +1,34 @@
+import hashlib
+import json
+import time
+
 class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
 
-  def new_block(self):
-    # Generate a new block and add it to the chain.
-    pass
+    # Generate the genesis block
+    self.new_block(previous_hash=1, proof=100)
+
+  def new_block(self, proof, previous_hash=None):
+    """
+    Generate a new block in the blockchain.
+    :param proof: <int> proof obtained from the proof-of-work algorithm
+    :param previous_hash: (optional) <str> hash of the previous block
+    :return: <dict> new block
+    """
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash or self.hash(self.chain[-1]),
+    }
+    # Reset current transaction list
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
 
   def new_transaction(self, sender, recipient, amount):
     """
@@ -24,8 +47,14 @@ class Blockchain(object):
 
   @staticmethod
   def hash(block):
-    # Hash the block
-    pass
+    """
+    Create a SHA-256 hash of a block
+    :param block: <dict> block
+    :return: <str> block
+    """
+    # The dictionary (dictionary type object) must always be sorted. Otherwise, it will be an inconsistent hash.
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
   @property
   def last_block(self):
