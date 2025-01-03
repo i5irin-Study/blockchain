332713a968e89e6274cc5f8864c78afae3d78705
i5irin <contact@i5ir.in>
2025-01-03 21:09:07 +0900

    feat: add new_transaction method

--- src/blockchain.py
+++ src/blockchain.py
@@ -7,9 +7,20 @@ class Blockchain(object):
     # Generate a new block and add it to the chain.
     pass
 
-  def new_transaction(self):
-    # Add a transaction to the list
-    pass
+  def new_transaction(self, sender, recipient, amount):
+    """
+    Create a new transaction to add to the next block to be mined
+    :param sender: <str> sender's address
+    :param recipient: <str> recipient's address
+    :param amount: <int> amount
+    :return: <int> address of the block containing this transaction
+    """
+    self.current_transactions.append({
+      'sender': sender,
+      'recipient': recipient,
+      'amount': amount,
+    })
+    return self.last_block['index'] + 1
 
   @staticmethod
   def hash(block):
